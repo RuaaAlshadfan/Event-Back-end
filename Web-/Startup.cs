fe76e30550ac4f29712becbf67244c597580112f
Rua'a Alshadfan <113802097+RuaaAlshadfan@users.noreply.github.com>
2024-01-06 15:51:40 +0300

    Add files via upload

---
+++ Web-/Startup.cs
@@ -0,0 +1,54 @@
+﻿using DataAccessLayer.DataContext;
+using DataAccessLayer.Repositories;
+using DataAccessLayer.Repositories.Interface;
+using Microsoft.EntityFrameworkCore;
+using ServicesLayer.Services;
+using ServicesLayer.Services.Interface;
+
+namespace Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        public IConfiguration Configuration { get; }
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddDbContext<EventContext>(options => options.UseSqlServer("name=ConnectionStrings:EventContext"));
+            services.AddMvc();
+            services.AddTransient<IEventRepository, EventRepository>();
+            services.AddTransient<IEventService, EventService>();
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ICustomerService, CustomerService>();
+            services.AddTransient<IRoleRepository, RoleRepository>();
+            services.AddTransient<IRoleService, RoleService>();
+            services.AddTransient<IUserRepository, UserRepository>();
+            services.AddTransient<IUserService, UserService>();
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseStaticFiles();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=List}/{id?}");
+            });
+        }
+    }
+}
